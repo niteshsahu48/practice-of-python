5f982fee06cd73074462532bfbce9be04c285c29
Nitesh <nitesh08815@gmail.com>
2024-11-12 14:16:43 +0530

    its find radius

--- radius.py
+++ radius.py
@@ -1,9 +1,11 @@
 def radius (value):
-	for num in range (10,31):
-		if num%(value)==0:
-			area_of_circle=3.14*(num*num)
-			circumfrence_of_circle=2*3.14*num
-			print ("area of circle=",(area_of_circle),"circumfrence of circle=",(circumfrence_of_circle))
+	
+	if num%(value)==0:
+		area_of_circle=3.14*(num*num)
+		circumfrence_of_circle=2*3.14*num
+		print ("area of circle=",(area_of_circle),"circumfrence of circle=",(circumfrence_of_circle))
 
 
 radius(3)
+for num in range (10,31):
+	print(num)
