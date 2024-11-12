6582a16ef2f1ee7c606abf6c5d04f31898bd9cd4
Nitesh <nitesh08815@gmail.com>
2024-11-12 14:15:03 +0530

    function of factorial

--- factorialfunction.py
+++ factorialfunction.py
@@ -4,13 +4,16 @@ def fact(n):
 		result=result*i
 	return result
 
-"""x=int(input("enter value:"))
-y=fact(x)
-print("factorial of {}={}".format(x,y))"""
-
 n = int(input("Enter Value of n : "))
 r = int(input("Enter Value of r : "))
 x = fact(n)/fact(n-r)
 y = fact(n)/(fact(n-r) * fact(r))
 print("Permutation of {} over {} = {}".format(n,r,x))
 print("Combination of {} over {} = {}".format(n,r,y))
+
+
+"""x=int(input("enter value:"))
+y=fact(x)
+print("factorial of {}={}".format(x,y))"""
+
+
