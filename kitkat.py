ba603584a6b959362aa908f6cc47e7e0ca5184f0
Nitesh <nitesh08815@gmail.com>
2024-11-12 14:32:44 +0530

    it file importing

---
+++ kitkat.py
@@ -0,0 +1,12 @@
+def plus(var1,var2):
+	var=var1+var2
+	print(var)
+def sub(var1,var2):
+	var=var1-var2
+	print(var)
+def mult(var1,var2):
+	var=var1*var2
+	print(var)
+def div (var1,var2):
+	var=var1/var2
+	print(var)
