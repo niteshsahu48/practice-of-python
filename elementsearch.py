a5e3810497a9f55fc0b4a1c250e0d8ae1aad09e0
Nitesh <nitesh08815@gmail.com>
2024-11-14 18:11:12 +0530

    this find element by <,>

--- elementsearch.py
+++ elementsearch.py
@@ -1,7 +1,9 @@
 user=int(input("how many value in list:-"))
 user1=int(input("Enter the value of greater than:-"))
+
 temp=[]
 add=[]
+add1=[]
 for i in range(1,user+1):
 	num=int(input('numbers:-'))
 	temp.append(num)
@@ -9,5 +11,8 @@ print("A list is:-",temp)
 for i in range (len(temp)):
 	if temp[i]>=user1:
 		add.append(temp[i])
+	elif temp[i]<=user1:
+		add1.append(temp[i])
 print(add)
-
+print(add1)
+	
