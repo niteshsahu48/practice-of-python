daddce0331d694748af217f05696560069adc8f5
Nitesh <nitesh08815@gmail.com>
2024-11-14 18:15:19 +0530

    this can find secound largest value

---
+++ secondbig.py
@@ -0,0 +1,54 @@
+"""user=int(input("Enter how many numbers in list:-"))
+list1=[]
+for i in range (1,user+1):
+	user1=int(input("Enter values:-"))
+	list1.append(user1)
+	list1.sort()
+	
+print(list1)
+list1.remove(max(list1))
+print(max(list1))"""
+
+
+"""for i in range (len(list1)):
+	if list1[i]==list1[-2]:
+		pass
+print(list1[i])
+"""
+
+
+
+"""user=int(input("Enter how many numbers in list:-"))
+list1=[]
+list2=[]
+
+for i in range (1,user+1):
+	user1=int(input("Enter values:-"))
+	list1.append(user1)
+list1.sort()
+	
+print(list1)
+
+list2.append(max(list1))
+list1.remove(max(list1))
+#print(max(list1))	
+for i in (list1):
+   	if max(list1)==max(list2):
+		list1.remove(max(list1))
+print(max(list1))	
+			"""
+user=int(input("Enter how many numbers in list:-"))
+list1=[]
+for i in range (1,user+1):
+	user1=int(input("Enter values:-"))
+	list1.append(user1)
+list1.sort()
+for i in range(len(1,user+1)-1):
+	if (list1[(-1)])!=(len(list1)-1):
+		print(list1)
+
+
+			
+
+
+			
