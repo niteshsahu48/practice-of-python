606b17657a2a9ad89412f3651cd326a65a67b67b
Nitesh <nitesh08815@gmail.com>
2024-11-15 16:31:48 +0530

    its list number are add

---
+++ numplus.py
@@ -0,0 +1,67 @@
+"""def numplus(user):
+	for i in range (len(user)):	
+		temp=0
+		while user[i]!=0:
+			digit=user[i]%10
+			user[i]=user[i]//10
+			temp=digit+temp
+		list1.append(temp)
+	print(list1)
+			
+user=[10,12,15,14]
+list1=[]
+numplus(user)"""
+		
+"""num=1235
+add=0
+for i in str(num):
+	add=add+int(i)
+print(add)"""
+add=12345
+temp=0
+var2=[]
+var=[]
+while add!=0:
+	digit=add%10
+	var2.append(digit)
+	print(var2)
+"""	sum1=digit//10
+	temp=sum1+temp
+	var.append(temp)
+print(var)
+"""
+
+
+
+
+
+
+
+
+
+"""user=512
+temp=0
+while user!=0:
+	print(user)
+	digit=user%10
+	print("reminder:-",digit)
+	user=user//10
+	print("less user:-",user)
+	temp=digit+temp
+	print("sum is:-",temp)"""
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
