dc0344dd6777aa23d9e7fc5c744bbfe9f83c81f6
Nitesh <nitesh08815@gmail.com>
2024-11-16 18:37:26 +0530

    this is backspace

---
+++ backspace.py
@@ -0,0 +1,17 @@
+string2="#abcde#fh#ggh####kjk#ggiu"
+var=""
+add=[]
+for i in range(len(string2)):
+	if string2[i]!="#":
+		add.append(string2[i])
+	elif string2[i]=="#":
+		var=i-1
+		if len(add)!=0 and string2[i+1]!=string2[i]:
+			var1=add.pop()
+	#print("The output string is:",add)
+var1="".join(add)
+print(var1)
+
+
+
+
