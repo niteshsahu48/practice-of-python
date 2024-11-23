6412d8a08e103b69c48656b844f8d5708ae9155a
Nitesh <nitesh08815@gmail.com>
2024-11-23 17:54:58 +0530

    sort values

---
+++ sort.py
@@ -0,0 +1,16 @@
+"""list1=[10,40,50,30,50,20]
+for i in range(0,len(list1)):
+	for j in range(i+1,len(list1)):
+		if list1[i]>=list1[j]:
+			list1[i],list1[j]=list1[j],list1[i]
+print(list1)"""
+
+list1=[10,50,20,30]
+for i in range (len(list1)):
+	
+
+
+
+
+	
+
