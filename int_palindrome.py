fa7a12f1720539ba2774a193a745ec2b00ca8913
Nitesh <nitesh08815@gmail.com>
2024-11-25 18:51:59 +0530

    int palindrome

---
+++ int_palindrome.py
@@ -0,0 +1,14 @@
+var1=int(input("Enter number for palindrome check:-"))
+temp = var1
+reverse = 0
+while var1!=0:
+	digit = var1%10
+	var1//=10
+	reverse=reverse*10+digit
+	print(reverse)
+	if temp==reverse:
+		print("True")
+	else:
+		print("False")
+	
+	
