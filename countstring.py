367a4507c12608621e437e10fd00ea0d091fdc06
Nitesh <nitesh08815@gmail.com>
2024-11-27 17:38:01 +0530

    string count

--- countstring.py
+++ countstring.py
@@ -1,15 +1,17 @@
-def add(store):
-	result=""
-	old=""
-	for i in store:
-		if i not in result:
-			count=store.count(i)	
-			result+=i
-			temp=f"{i}{count}"
-			old+=temp
-	return(old)	
+"""def add(store):
+		result=""
+		old=""
+		for i in store:
+			if i not in result:
+				count=store.count(i)	
+				result+=i
+				temp=f"{i}{count}"
+				old+=temp
+		return(old)	
+	
+	store=input("enter character:-")
+	
+	kill=add(store)
+	print(kill)	"""	
 
-store=input("enter character:-")
 
-kill=add(store)
-print(kill)		
