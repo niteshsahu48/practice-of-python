60b6cc6423abbc6a7b84ad915d1815b552538a92
Nitesh <nitesh08815@gmail.com>
2024-11-27 17:42:47 +0530

    alfa string count

--- newstring_count.py
+++ newstring_count.py
@@ -3,14 +3,9 @@ Empty=""
 count=1
 for i in range (1,len(user)):
 	if user[i]==user[i-1]:
-		print(user[i])
-		print(user[i-1])
 		count+=1
-		print(count)
 	else:
 		Empty+=(user[i-1]+str(count))
-		print(Empty)
 		count=1
-		print(count)
-#Empty+=(user[-1]+str(count))
+Empty+=(user[-1]+str(count))
 print(Empty)
