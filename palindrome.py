3f86df1cf119604f17d6939724f5a0b2d276f771
Nitesh <nitesh08815@gmail.com>
2024-11-28 16:25:04 +0530

    palindrome string

--- palindrome.py
+++ palindrome.py
@@ -1,7 +1,9 @@
 print("welcome to this program")
-var1=input("Enter words for check palindrome:-")
+#var1=input("Enter words for check palindrome:-")
+var1=-121
+var1=str
 var2=""
-for i in var1:
+for i in range (len(var1)):
 	var2=i+var2
 	
 if var2==var1:
