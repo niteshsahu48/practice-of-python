187581985ae2b7ad5321880e33fe49426c236d4a
Nitesh <nitesh08815@gmail.com>
2024-12-07 22:53:38 +0530

    roman to int

---
+++ roman_to_int.py
@@ -0,0 +1,21 @@
+value="MCMXCIV"
+roman={
+    "I" :1,
+    "V" :5,
+    "X" :10,
+    "L" :50,
+    "C" :100,
+    "D" :500,
+    "M" :1000,
+    }
+a=0
+for i in range (len(value)):
+    if i>i+1:
+        a+=i
+
+    
+        
+    print()
+        
+        
+    
