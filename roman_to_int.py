032abffaf5cfc781c7ca8297fd44271e4160d704
Nitesh <nitesh08815@gmail.com>
2024-12-09 16:46:08 +0530

    roman to int

--- roman_to_int.py
+++ roman_to_int.py
@@ -1,21 +1,21 @@
-value="MCMXCIV"
-roman={
-    "I" :1,
-    "V" :5,
-    "X" :10,
-    "L" :50,
-    "C" :100,
-    "D" :500,
-    "M" :1000,
-    }
+r="MCMXCIV"
+roman={"I" :1,"V" :5,"X" :10,"L" :50,"C" :100,"D" :500,"M" :1000}
 a=0
-for i in range (len(value)):
-    if i>i+1:
-        a+=i
+for i in range(len(r)):
+    if i+1<len(r) and roman[r[i]]<roman[r[i+1]]:
+        a-=roman[r[i]]
+    else:
+        a+=roman[r[i]]
+print(a)
+
+        
+
+   
+        
 
     
         
-    print()
+   
         
         
     
